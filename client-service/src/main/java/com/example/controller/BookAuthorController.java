25d61ff6e8a82101ce2891cbabf79b2816320a37
Bharath M <48067189+bharath79@users.noreply.github.com>
2022-06-25 21:50:55 +0530

    Add files via upload

---
+++ client-service/src/main/java/com/example/controller/BookAuthorController.java
@@ -0,0 +1,43 @@
+package com.example.controller;
+
+import com.example.service.BookAuthorClientService;
+import com.google.protobuf.Descriptors;
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@AllArgsConstructor
+public class BookAuthorController {
+
+    BookAuthorClientService bookAuthorClientService;
+
+    @GetMapping("/author/{id}")
+    public Map<Descriptors.FieldDescriptor, Object> getAuthor(@PathVariable String id) {
+        return bookAuthorClientService.getAuthor(Integer.parseInt(id));
+    }
+
+    @GetMapping("/book/{authorId}")
+    public List<Map<Descriptors.FieldDescriptor,Object>> getBooksByAuthor(@PathVariable String authorId) throws InterruptedException {
+        return bookAuthorClientService.getBooksByAuthor(Integer.parseInt(authorId));
+    }
+
+    @GetMapping("/book")
+    public Map<String,Map<Descriptors.FieldDescriptor,Object>> getExpensiveBook() throws InterruptedException {
+        return bookAuthorClientService.getExpensiveBook();
+    }
+
+    @GetMapping("/book/author/{gender}")
+    public List<Map<Descriptors.FieldDescriptor,Object>> getBooksByAuthorGender(@PathVariable String gender) throws InterruptedException {
+        return bookAuthorClientService.getBooksByAuthorGender(gender);
+    }
+
+    @GetMapping("/health")
+    public String health(){
+        return "ping";
+    }
+}
